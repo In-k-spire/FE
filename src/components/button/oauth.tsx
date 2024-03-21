027d38adcf6d69167df916dd83d031f706c20cff
dhlee <donghuni512@gmail.com>
2024-03-21 17:05:22 +0900

    Style:: 메인 디자인 수정

--- src/components/button/oauth.tsx
+++ src/components/button/oauth.tsx
@@ -3,7 +3,7 @@ import BtnProps from "@/type/button/index.type";
 export const OAuthBtn = ({ children, className, ...props }: BtnProps) => {
   return (
     <button
-      className={`flex h-12 w-12  items-center justify-center rounded-full border border-grayscale/20 lg:h-14 lg:w-14  xl:h-20 xl:w-20 ${className}`}
+      className={`flex h-12 w-12  items-center justify-center rounded-full border border-grayscale/20 lg:h-14 lg:w-14  xl:h-16 xl:w-16 ${className}`}
     >
       {children}
     </button>
