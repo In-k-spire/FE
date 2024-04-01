5040ec30d7e80ab62355129dc9dcff8ffd2c5599
dhlee <donghuni512@gmail.com>
2024-04-01 17:25:54 +0900

    Fix:: onclick 추가

--- src/components/button/oauth.tsx
+++ src/components/button/oauth.tsx
@@ -3,6 +3,7 @@ import BtnProps from "@/type/button";
 export const OAuthBtn = ({ children,color, ...props }: BtnProps) => {
   return (
     <button
+      onClick={props.onClick}
       className={`flex h-12 w-12  items-center justify-center rounded-full border border-grayscale/20 lg:h-14 lg:w-14  xl:h-16 xl:w-16 bg-${color}`}
     >
       {children}
