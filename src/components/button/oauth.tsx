0341ca3715cc1beca377866a2b91c85d84911816
이동훈 <202110309@bssm.hs.kr>
2024-04-02 22:48:49 +0900

    Fix:: client side 문제 수정

--- src/components/button/oauth.tsx
+++ src/components/button/oauth.tsx
@@ -1,16 +1,29 @@
 import { server } from "@/api/instance/instance";
-import { ReactNode } from "react";
-export const OAuthBtn = async ({ children , provider} : {children : ReactNode, provider : 'github' | 'google'} ) => {
+import { AxiosResponse } from "axios";
+import { ReactNode, useEffect, useState } from "react";
+export const OAuthBtn = ({
+  children,
+  provider,
+}: {
+  children: ReactNode;
+  provider: "github" | "google";
+}) => {
   const COLOR = {
-    github : "bg-black",
-    google : "bg-white"
-  }
-  const {data} = await server.get(
-    `/auth/${provider}/uri?redirectUri=localhost:3000/oauth?provider=${provider}`,
-  );
+    github: "bg-black",
+    google: "bg-white",
+  };
+  const [uri, setUri] = useState<string>("");
+  useEffect(() => {
+    server
+      .get(
+        `/auth/${provider}/uri?redirectUri=localhost:3000/oauth?provider=${provider}`,
+      )
+      .then((res: AxiosResponse) => setUri(res.data.uri))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <a
-      href={data.uri}
+      href={uri}
       className={`flex h-12 w-12  items-center justify-center rounded-full border border-grayscale/20 lg:h-14 lg:w-14  xl:h-16 xl:w-16 ${COLOR[provider]}`}
     >
       {children}
