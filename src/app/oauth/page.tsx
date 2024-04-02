ee1c17c99a23f40568cd67622213b2df5dada7e7
이동훈 <202110309@bssm.hs.kr>
2024-04-02 22:48:33 +0900

    Add:: 로그인 로딩 구현

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,13 +1,21 @@
-import { server } from "@/api/instance/instance"
+"use client";
 import { useLoginMutation } from "@/service/user/mutations";
 import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+export default function OAuth() {
+  const { loginMutate } = useLoginMutation();
+  const params = useSearchParams();
+  const code = params.get("code");
+  const provider = params.get("provider");
+  useEffect(() => {
+    loginMutate({ code, provider });
+  }, []);
 
-export const OAuth = async () => {
-    const {loginMutate } = useLoginMutation()
-    const params = useSearchParams();
-    const code = params.get('code');
-    const provider = params.get('provider');
-    loginMutate({code,provider})
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="text-4xl font-medium">
+        로그인 중입니다. 잠시만 기다려주세요.
+      </div>
+    </div>
+  );
 }
-
-
--- src/service/user/mutations.ts
+++ src/service/user/mutations.ts
@@ -1,14 +1,17 @@
-import { useMutation } from "@tanstack/react-query"
-import { login } from "./api"
-import { LoginProps } from "@/type/user/user"
-import { AxiosResponse } from "axios"
+import { useMutation } from "@tanstack/react-query";
+import { login } from "./api";
+import { LoginProps } from "@/type/user/user";
+import { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
 
 export const useLoginMutation = () => {
-    const { mutate: loginMutate, ...restMutation } = useMutation({
-      mutationFn: (props: LoginProps) => login(props),
-      onSuccess: (res: AxiosResponse) => {
-        console.log(res);
-      },
-    });
-    return {loginMutate, ...restMutation}
-}
+  const router = useRouter();
+  const { mutate: loginMutate, ...restMutation } = useMutation({
+    mutationFn: (props: LoginProps) => login(props),
+    onSuccess: (res: AxiosResponse) => {
+      router.push("/");
+    },
+    onError: (err: AxiosError) => router.push("/"),
+  });
+  return { loginMutate, ...restMutation };
+};
