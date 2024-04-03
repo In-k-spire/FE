198d3b58902d479429d77ef7990bc98c0ec4f77a
dhlee <donghuni512@gmail.com>
2024-04-03 15:07:26 +0900

    Update:: Login pending 핸들링

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import { useLoginMutation } from "@/service/user/mutations";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 export default function OAuth() {
-  const { loginMutate } = useLoginMutation();
+  const { loginMutate, isPending  } = useLoginMutation();
+  const router = useRouter();
   const params = useSearchParams();
   const code = params.get("code");
   const provider = params.get("provider");
   useEffect(() => {
     loginMutate({ code, provider });
+    if (!isPending) router.push("/");
   }, []);
-
+  
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="text-4xl font-medium">
-        로그인 중입니다. 잠시만 기다려주세요.
-      </div>
+        <div className="text-4xl font-medium">
+          로그인 중입니다. 잠시만 기다려주세요.
+        </div>
     </div>
   );
 }
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,8 +1,10 @@
 import { useUserQuery } from "@/service/user/queries";
 import { useUserStore } from "@/store/user"
 
-export const useUser = () => {
+const useUser = () => {
     const [user, setUser] = useUserStore();
     const {data}  = useUserQuery();
     console.log(data)
 }
+
+export default useUser;
