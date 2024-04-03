4a8c8c6564032cbe1b1728c7042ade4f25dbc400
dhlee <donghuni512@gmail.com>
2024-04-03 15:51:09 +0900

    Update::OAuth client -> server 컴포넌트 변경

--- src/components/button/oauth.tsx
+++ src/components/button/oauth.tsx
@@ -1,8 +1,6 @@
-"use client"
 import { server } from "@/api/instance/instance";
-import { AxiosResponse } from "axios";
-import { ReactNode, useEffect, useState } from "react";
-export const OAuthBtn = ({
+import { ReactNode } from "react";
+export const  OAuthBtn = async  ({
   children,
   provider,
 }: {
@@ -13,18 +11,12 @@ export const OAuthBtn = ({
     github: "bg-black",
     google: "bg-white",
   };
-  const [uri, setUri] = useState<string>("");
-  useEffect(() => {
-    server
-      .get(
-        `/auth/${provider}/uri?redirectUri=localhost:3000/oauth?provider=${provider}`,
-      )
-      .then((res: AxiosResponse) => setUri(res.data.uri))
-      .catch((err) => console.log(err));
-  }, []);
+  const { data } = await server.get(
+    `/auth/${provider}/uri?redirectUri=localhost:3000/oauth?provider=${provider}`,
+  );
   return (
     <a
-      href={uri}
+      href={data.uri}
       className={`flex h-12 w-12  items-center justify-center rounded-full border border-grayscale/20 lg:h-14 lg:w-14  xl:h-16 xl:w-16 ${COLOR[provider]}`}
     >
       {children}
