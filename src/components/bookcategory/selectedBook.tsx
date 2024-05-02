4da62975e8d558493842f14d58e617ea2efcd221
이동훈 <202110309@bssm.hs.kr>
2024-05-02 19:28:31 +0900

    Update:: 책 자세히보기 변경

--- src/components/bookcategory/selectedBook.tsx
+++ src/components/bookcategory/selectedBook.tsx
@@ -1,22 +1,32 @@
 import { BookType } from "@/type/book";
 import BookLayout from "../book/bookLayout";
 import { Rating } from "../share/rating";
+import BtnLayout from "../button";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
 export const SelectedBook = ({ book }: { book: BookType }) => {
   return (
-    <div className="group relative">
-      <div className="transform-preserve3d relative z-10 origin-top-left border rounded-lg border-secondary shadow-lg ">
+    <div className="group/book relative">
+      <div className="transform-preserve3d relative origin-top-left rounded-lg border border-secondary shadow-lg ">
         <BookLayout
           image={book?.image}
-          className="backface-invisible z-10"
+          className="backface-invisible z-10 "
           size="xl"
         />
-        <div className="absolute left-0 top-0 z-0 h-full w-full rounded-lg p-4 [transform:rotateY(-180deg)] bg-white">
-          <Rating star={5}/>
+        <div className="absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center rounded-lg p-4 opacity-0 duration-300 group-hover/book:bg-[rgba(255,255,255,0.7)] group-hover/book:opacity-100 ">
+          <Link href={``}>
+            <BtnLayout
+              size="md"
+              color="primary"
+              rounded="rounded-md"
+              className="group"
+            >
+              독후감 보기
+              <FaArrowRight className="duration-300 group-hover:translate-x-2" />
+            </BtnLayout>
+          </Link>
         </div>
-      </div>
-      <div className="bg-white absolute left-0 top-0 w-full p-4 h-full overflow-auto border rounded-lg border-secondary">
-        {book?.description}
       </div>
     </div>
   );
