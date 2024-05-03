2795f312690eac2d5af78bf0c2de2ac62757c228
이동훈 <202110309@bssm.hs.kr>
2024-05-03 22:09:21 +0900

    Feat:: page transition api 사용

--- src/app/report/[category]/page.tsx
+++ src/app/report/[category]/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 import BookList from "@/components/bookcategory/bookList";
 import { SelectedBook } from "@/components/bookcategory/selectedBook";
-
 import { useNaverBooks } from "@/service/naver/queries";
 import { useState } from "react";
 
 const CategoryDetail = () => {
   const [selectedBookId, setSelectedBookId] = useState(0);
   const { data: books, isLoading } = useNaverBooks("과학", 50);
-
+  const handleBookChange = (bookIdx: number) => {
+    document.startViewTransition(() => {
+      setSelectedBookId(bookIdx);
+    });
+  };
   return (
     <div
       className={`flex h-full w-full flex-col items-center justify-between gap-8 lg:flex-row lg:gap-0`}
     >
-      <BookList setSelectedBookId={setSelectedBookId} books={books} />
+      <BookList setSelectedBookId={handleBookChange} books={books} />
       {!isLoading ? <SelectedBook book={books[selectedBookId]} /> : null}
     </div>
   );
