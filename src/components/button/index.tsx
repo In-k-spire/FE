4c2c4094c3fb95d62231a8e9e9851e98c82e8d01
이동훈 <202110309@bssm.hs.kr>
2024-05-09 17:08:37 +0900

    Feat:: 버튼 disabled기능 생성

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,21 +1,32 @@
 import BtnProps from "@/type/button";
 
-export const BtnLayout = ({ children, size = "md", rounded = "rounded-none", filled = 'filled', color ,className,  ...props }: BtnProps) => {
+export const BtnLayout = ({
+  children,
+  size = "md",
+  rounded = "rounded-none",
+  filled = "filled",
+  color,
+  className,
+  ...props
+}: BtnProps) => {
   const SIZE = {
-    sm : 'px-4  text-sm lg:text-base',
-    md : 'px-8 text-base lg:text-lg',
-    lg: 'px-12 text-lg lg:text-xl',
-    xl : 'px-16 text-xl lg:text-2xl'
-  }
+    sm: "px-4  text-sm lg:text-base",
+    md: "px-8 text-base lg:text-lg",
+    lg: "px-12 text-lg lg:text-xl",
+    xl: "px-16 text-xl lg:text-2xl",
+  };
   const COLOR = {
-    filled : `text-white bg-${color} `,
-    ghost : `text-${color} border bg-transparent border-${color} hover:bg-${color} hover:text-white`
-  }
-
+    filled: `text-white bg-${color} `,
+    ghost: `text-${color} border bg-transparent border-${color} hover:bg-${color} hover:text-white`,
+  };
 
   return (
     <button
-      className={`flex items-center ${rounded} max-w-full whitespace-nowrap gap-2 duration-300  py-2 justify-center shadow-lg ${COLOR[filled]} ${SIZE[size]} w-fit h-fit ${className}`}
+      className={`flex ${
+        props.disabled ? "pointer-events-none opacity-50" : "opacity-100"
+      } items-center ${rounded} max-w-full justify-center gap-2 whitespace-nowrap  py-2 shadow-lg duration-300 ${
+        COLOR[filled]
+      } ${SIZE[size]} h-fit w-fit ${className}`}
       {...props}
     >
       {children}
