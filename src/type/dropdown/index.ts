6ff276df72a4ae9a89c75411e8e68f123c8a3230
dohi0512 <donghuni512@gmail.com>
2024-09-30 16:16:46 +0900

    Chore:: 안쓰는 타입 삭제

--- src/type/dropdown/index.ts
+++ src/type/dropdown/index.ts
@@ -2,7 +2,6 @@ import { ReactNode } from "react";
 import { CommonProps } from "../common";
 
 export interface DropDownProps extends CommonProps {
-  color: string;
   children: ReactNode;
   title: string;
 }
