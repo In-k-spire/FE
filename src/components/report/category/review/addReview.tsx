749341c1ad7f9e5dbe49e6a93f1299db2b4e19b3
dohi0512 <donghuni512@gmail.com>
2024-09-30 16:18:50 +0900

    Style:: background색상 수정

--- src/components/report/category/review/addReview.tsx
+++ src/components/report/category/review/addReview.tsx
@@ -7,16 +7,16 @@ const AddReview = () => {
       <span className=" text-2xl font-bold">독후감 작성</span>
       <InputLayout
         sizes="full"
-        className=" rounded-md border border-gray-300 bg-background/white focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+        className=" rounded-md border border-gray-300 bg-white focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
         placeholder="한줄평을 입력해주세요"
       />
       <textarea
         placeholder="독후감을 작성해주세요"
-        className="h-48 rounded-md border border-gray-300 bg-background/white p-4 outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+        className="h-48 rounded-md border border-gray-300 bg-white p-4 outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
       />
       <InputLayout
         sizes="full"
-        className=" rounded-md border border-gray-300 bg-background/white focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+        className=" rounded-md border border-gray-300 bg-white focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
         placeholder="읽은 페이지 (예: 1p ~ 50p)"
       />
       <BtnLayout
--- src/components/report/category/review/reviewInfo.tsx
+++ src/components/report/category/review/reviewInfo.tsx
@@ -2,7 +2,7 @@ import BookLayout from "@/components/share/book";
 
 const ReviewInfo = () => {
   return (
-    <div className="flex flex-col rounded-md border border-gray-300 p-6 shadow-sm">
+    <div className="flex flex-col rounded-md border border-gray-300 bg-white p-6 shadow-sm">
       <span className="text-2xl font-bold">떨림과 울림</span>
       <span className="mt-2 text-sm text-gray-400">김상욱/동아시아</span>
       <div className="mt-12 flex gap-8">
