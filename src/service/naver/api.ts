c535d5fba2b25a842d103e1a7e865a14a2aca31e
dohi0512 <donghuni512@gmail.com>
2024-09-30 16:17:01 +0900

    Add:: query가 없으면 요청을 보내지 않음

--- src/service/naver/api.ts
+++ src/service/naver/api.ts
@@ -2,6 +2,7 @@ import { naver } from "@/api/instance/instance";
 import type { GetBooks } from "@/type/naver";
 
 export const getBooks = async ({ query, display }: GetBooks) => {
+  if (!query) return [];
   const { data } = await naver.get("/", {
     params: { query: query, display: display },
   });
