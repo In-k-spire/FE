036f94fce4d603da59683dd1b9b2dba2530725db
dohi0512 <donghuni512@gmail.com>
2024-10-02 14:13:45 +0900

    delete

--- src/hooks/useBook.ts
+++
@@ -1,9 +0,0 @@
-import { useBookQuery } from "@/service/book/queries";
-
-const useBook = (id: number) => {
-  const { data, isError } = useBookQuery(id);
-
-  return { book: data };
-};
-
-export default useBook;
