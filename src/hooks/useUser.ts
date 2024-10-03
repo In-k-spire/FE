7a129c27288b8f8438b3c1f15123689bd97e3fa7
dohi0512 <donghuni512@gmail.com>
2024-10-03 23:15:01 +0900

    Add:: 로그아웃시 홈화면으로

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -2,11 +2,13 @@ import { Storage } from "@/api/storage/storage";
 import { useUserQuery } from "@/service/user/queries";
 import { useUserStore } from "@/store/user";
 import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const useUser = () => {
   const queryClient = useQueryClient();
   const [user, setUser] = useUserStore();
+  const router = useRouter();
   const { data } = useUserQuery();
   useEffect(() => {
     setUser(data);
@@ -16,6 +18,7 @@ const useUser = () => {
     Storage.removeItem("accessToken");
     Storage.removeItem("refreshToken");
     queryClient.invalidateQueries({ queryKey: ["user"] });
+    router.replace("/");
   };
   return { user, logout };
 };
