c1e8cd68f4129813226d4a9a02785f138ef4cc50
dohi0512 <donghuni512@gmail.com>
2024-10-03 20:25:01 +0900

    Add:: 404 추가

--- src/hooks/useApiError.ts
+++ src/hooks/useApiError.ts
@@ -5,17 +5,18 @@ import { toast } from "react-toastify";
 const useApiError = () => {
   const router = useRouter();
   const handleError = (error: AxiosError) => {
-    console.log("dawd", error);
     if (error.response) {
       let errorMessage = "";
       const errorCode = error.response.status;
       if (errorCode === 401) errorMessage = "유저의 권한이 없습니다.";
       else if (errorCode === 403) errorMessage = "차단된 접근입니다.";
+      else if (errorCode === 404) errorMessage = "데이터를 찾을 수 없습니다.";
       else if (errorCode === 429)
         errorMessage = "너무 많이 요청했습니다. 잠시 뒤에 다시 시도해주세요.";
       else errorMessage = "서버에 알 수 없는 오류가 발생했습니다.";
       toast.error(errorMessage, { toastId: "error" });
       if (errorCode == 401) router.replace("/login");
+      else router.replace("/");
     }
   };
 
