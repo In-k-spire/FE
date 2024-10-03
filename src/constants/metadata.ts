a6f631b848ecdab291c50e3d80384d10a0761ece
dohi0512 <donghuni512@gmail.com>
2024-10-03 19:59:24 +0900

    Rename:: 폴더명 변경

---
+++ src/constants/metadata.ts
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+const CommonMetadata: Metadata = {
+  description: "독후감 기록 사이트",
+  openGraph: {
+    title: "Inkspire",
+    description: "독후감 기록 사이트",
+    url: "https://nextjs.org",
+    siteName: "Inkspire",
+    locale: "ko_KR",
+    type: "website",
+  },
+};
+
+export default CommonMetadata;
