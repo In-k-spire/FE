746d8a42f033784d5431b90ca05f4dfeac7c0455
dohi0512 <donghuni512@gmail.com>
2024-10-03 20:00:31 +0900

    Chore:: 정렬

--- src/components/share/shelf.tsx
+++ src/components/share/shelf.tsx
@@ -1,10 +1,10 @@
 export const Shelf = () => {
   return (
     <div className="relative flex w-full flex-col items-center ">
-      <div className="border-r-shelfColor  triangle absolute left-0 -translate-x-full border-l-[transparent]" />
-      <div className="bg-shelfColor custom-shadow h-8 lg:h-10 2xl:h-12 w-full"></div>
-      <div className="border-l-shelfColor  triangle absolute right-0 translate-x-full  border-r-[transparent]" />
-      <div className=" h-4 w-[calc(100%+4.5rem)] lg:w-[calc(100%+5.5rem)] 2xl:w-[calc(100%+6.5rem)] rounded-full bg-[#e0dac1] "></div>
+      <div className="triangle  absolute left-0 -translate-x-full border-l-[transparent] border-r-shelfColor" />
+      <div className="custom-shadow h-8 w-full bg-shelfColor lg:h-10 2xl:h-12" />
+      <div className="triangle  absolute right-0 translate-x-full border-l-shelfColor  border-r-[transparent]" />
+      <div className=" h-4 w-[calc(100%+4.5rem)] rounded-full bg-[#e0dac1] lg:w-[calc(100%+5.5rem)] 2xl:w-[calc(100%+6.5rem)] " />
     </div>
   );
 };
