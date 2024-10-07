bb60b72ae21b056b1a58039c15f1d4b4908c2e92
dohi0512 <donghuni512@gmail.com>
2024-10-07 20:19:42 +0900

    Refactoring:: api rename

--- src/app/review/[id]/page.tsx
+++ src/app/review/[id]/page.tsx
@@ -2,11 +2,11 @@
 import AddReview from "@/components/category/category/review/addReview";
 import ReviewInfo from "@/components/category/category/review/reviewInfo";
 import ReviewList from "@/components/category/category/review/reviewList";
-import { useBookQuery } from "@/service/book/queries";
+import { useBook } from "@/service/book/queries";
 
 const BookDetail = ({ params }: { params: { id: number } }) => {
   const { id } = params;
-  const { book, isLoading } = useBookQuery(id);
+  const { book, isLoading } = useBook(id);
   return (
     <div className="flex flex-col gap-8">
       <ReviewInfo book={book} />
