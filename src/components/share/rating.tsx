f36a536384c0aba8ff5071bb4c6e5f3b4f21a561
dohi0512 <donghuni512@gmail.com>
2024-10-07 20:16:56 +0900

    delete

--- src/components/share/rating.tsx
+++
@@ -1,5 +0,0 @@
-import { FaRegStar } from "react-icons/fa";
-export const Rating = ({ star }: { star: number }) => {
-  const Star = [1, 2, 3, 4, 5].map((idx) => <FaRegStar key={idx} />);
-  return <div className="flex gap-1">{Star}</div>;
-};
