d00c33cad324656d8bb52c781755e98256c817fe
dohi0512 <donghuni512@gmail.com>
2024-10-07 20:51:12 +0900

    Style:: md 분기 추가

--- src/components/share/shelf.tsx
+++ src/components/share/shelf.tsx
@@ -1,10 +1,10 @@
 export const Shelf = () => {
   return (
-    <div className="relative flex w-full flex-col items-center ">
+    <div className="relative flex w-full flex-col items-center">
       <div className="triangle  absolute left-0 -translate-x-full border-l-[transparent] border-r-shelfColor" />
-      <div className="custom-shadow h-8 w-full bg-shelfColor lg:h-10 2xl:h-12" />
+      <div className="custom-shadow h-6 w-full bg-shelfColor md:h-8 lg:h-10 2xl:h-12" />
       <div className="triangle  absolute right-0 translate-x-full border-l-shelfColor  border-r-[transparent]" />
-      <div className=" h-4 w-[calc(100%+4.5rem)] rounded-full bg-[#e0dac1] lg:w-[calc(100%+5.5rem)] 2xl:w-[calc(100%+6.5rem)] " />
+      <div className=" h-4  w-[calc(100%+3.5rem)] rounded-full bg-[#e0dac1] md:w-[calc(100%+4.5rem)] lg:w-[calc(100%+5.5rem)] 2xl:w-[calc(100%+6.5rem)] " />
     </div>
   );
 };
