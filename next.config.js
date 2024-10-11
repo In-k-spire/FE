e594964d980e909dc611eef17a6580d60ba7aae7
dohi0512 <donghuni512@gmail.com>
2024-10-11 16:39:43 +0900

    Fix:: webpack 경고 안보이게

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, options) => {
+    config.cache = false;
+    return config;
+  },
   reactStrictMode: false,
   images: {
     domains: ["shopping-phinf.pstatic.net"],
