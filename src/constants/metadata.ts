55346883fd65366c48fc9e502489084e03375125
dohi0512 <donghuni512@gmail.com>
2024-10-11 16:41:35 +0900

    Add:: metadatabase 추가

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,15 +1,15 @@
 import { Metadata } from "next";
-
 const CommonMetadata: Metadata = {
   description: "독후감 기록 사이트",
   openGraph: {
     title: "Inkspire",
     description: "독후감 기록 사이트",
-    url: "https://ink-spire.netlify.app/",
+    url: new URL("https://ink-spire.netlify.app/"),
     siteName: "Inkspire",
     locale: "ko_KR",
     type: "website",
   },
+  metadataBase: new URL("https://ink-spire.netlify.app/"),
 };
 
 export default CommonMetadata;
