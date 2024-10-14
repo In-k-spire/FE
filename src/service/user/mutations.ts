f0114a1c1b2955f4150fbdc905a0b1648d06a909
dohi0512 <donghuni512@gmail.com>
2024-10-14 23:43:14 +0900

    Add:: 로그아웃 성공시 홈으로

--- src/service/user/mutations.ts
+++ src/service/user/mutations.ts
@@ -2,9 +2,11 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "./api";
 import { deleteCookie } from "cookies-next";
 import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export const useLogoutMutation = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
   const { mutate } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
@@ -12,6 +14,7 @@ export const useLogoutMutation = () => {
       deleteCookie("refreshToken");
       queryClient.invalidateQueries({ queryKey: ["user"] });
       toast.success("로그아웃 성공!");
+      router.replace("/");
     },
   });
 
