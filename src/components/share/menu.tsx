68f798bb0d4904e2855c3d967cb96798e3e1447e
dohi0512 <donghuni512@gmail.com>
2024-10-14 23:43:47 +0900

    Fix:: 선반에 가려지는 문제 해결

--- src/components/share/menu.tsx
+++ src/components/share/menu.tsx
@@ -18,7 +18,7 @@ const Menu = ({ onEdit, onRemove, className }: IMenu) => {
     close();
   };
   return (
-    <div onClick={(e) => e.stopPropagation()}>
+    <div onClick={(e) => e.stopPropagation()} className="z-10">
       <div
         onClick={() => setIsOpen((prev) => !prev)}
         className={`${className} relative flex aspect-square   h-full cursor-pointer items-center justify-center rounded-md duration-200`}
